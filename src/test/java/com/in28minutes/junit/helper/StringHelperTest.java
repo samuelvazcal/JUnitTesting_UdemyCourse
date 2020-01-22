d0422954d2dc28d3987f738a92b19c76ec258952
Samuel Vazquez <samuel_vazquez@epam.com>
2020-01-22 13:26:52 -0600

    Added code refactoring in order to improve the first unit test

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -6,21 +6,23 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	StringHelper helper = new StringHelper();
+	
 	@Test
-	public void test() {
+	public void testTruncateAInFirst2Positions() {
 		//Creating an instance of my particular method to be tested
-		StringHelper helper = new StringHelper();
+
 		//Calling the first method to be tested
 		//actual is where the value from --- helper.truncateAInFirst2Positions("AACD") ---  is saved 
 		
 		//AACD => CD ACD=> CD CDEF=>CDEF CDAA => CDAA
-
 		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 		//There are two arguments, where:
 		//expected (the first one), actual (the second value)
 	}
-	//expected:<ABC[]> but was:<ABC[D]>
-	
+	@Test
+	public void testTruncateAInFirst2Positions2() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
 
 }
